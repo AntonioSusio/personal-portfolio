a047c37357ffd9c86f52ef2d719c115f08d2c41b
Antonio <antoniosusio@gmail.com>
2025-01-21 17:13:43 +0100

    Starting the header

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header () {
+    return (
+        <header>
+            <span className="header-title">My portfolio</span>
+            <button className="switch-language-button">Switch to Italian</button>
+        </header>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react"
+import Header from "../components/Header"
 
 function App() {
 
   return (
     <div className="site-wrapper">
-      <h1>test</h1>
+      <Header />
     </div>
   )
 }
