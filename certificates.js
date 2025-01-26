d56c6f14a14e51fe623db63c4c4fa6920c91e1bf
Antonio <antoniosusio@gmail.com>
2025-01-26 15:07:16 +0100

    Added certificates in certificates.js

--- certificates.js
+++ certificates.js
@@ -2,5 +2,25 @@ export default [
     {
         id: "1",
         image: "FrontEndCertificate.png"
+    },
+
+    {
+        id: "2",
+        image: "Html&CssCertificate.png"
+    },
+
+    {
+        id: "3",
+        image: "JavascriptCertificate.png"
+    },
+
+    {
+        id: "4",
+        image: "ReactCertificate.png"
+    },
+
+    {
+        id: "5",
+        image: "FromFigmaToCodeCertificate.png"
     }
 ]
