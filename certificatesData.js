1122baa62e1a6df60d39a02da54c6df1f1bb781b
Antonio <antoniosusio@gmail.com>
2025-01-27 10:00:12 +0100

    Started implement certificates section and carousel container

---
+++ certificatesData.js
@@ -0,0 +1,31 @@
+export default [
+    {
+        id: 1,
+        image: "FrontEndCertificate.png",
+        description: "Image representing Frontend career path certificate"
+    },
+
+    {
+        id: 2,
+        image: "Html&CssCertificate.png",
+        description: "Image representing Html and css certificate"
+    },
+
+    {
+        id: 3,
+        image: "JavascriptCertificate.png",
+        description: "Image representing Javascript certificate"
+    },
+
+    {
+        id: 4,
+        image: "ReactCertificate.png",
+        description: "Image representing React certificate"
+    },
+
+    {
+        id: 5,
+        image: "FromFigmaToCodeCertificate.png",
+        description: "Image representing From figma to code certificate"
+    }
+]
--- components/Certificates.jsx
+++ components/Certificates.jsx
@@ -1,12 +1,33 @@
 import React from "react"
-import certificates from "../certificates"
+import certificatesData from "../certificatesData"
+import { GrFormPrevious, GrFormNext } from "react-icons/gr";
 
 export default function Certificates() {
-    return (
-        <div className="carousel">
-            {certificates.map(certificate => (
-                <p>{certificate.id}</p>
-            ))}
+    const [certificates, setCertificates] = React.useState(certificatesData)
+
+    console.log(certificates)
+
+    const certificatesEl = certificates.map(certificate => (
+        <div 
+            key={certificate.id}
+            className={`${certificate.id === 1 ? "slide-visible" : "slide-hidden"} certificate-img-container`}>
+            <img 
+                src={`./src/assets/images/${certificate.image}`} 
+                alt={certificate.description} 
+            />
         </div>
+    ))
+
+    return (
+        <section className="certificates-section">
+            <h2 className="certificates-section-header">Certificates</h2>
+            <div className="carousel">
+                {certificatesEl}
+            </div>
+            <div className="carousel-buttons-container">
+                <GrFormPrevious className="carousel-btn" />
+                <GrFormNext className="carousel-btn"/>
+            </div>
+        </section>
     )
 }
