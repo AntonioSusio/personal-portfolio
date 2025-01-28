547fda930ae9692a7b5263b2274578dd4f033c8a
Antonio <antoniosusio@gmail.com>
2025-01-28 13:12:51 +0100

    added projext component and imported in main.jsx

--- components/Sections/Main.jsx
+++ components/Sections/Main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import About from "./About";
 import Certificates from "./Certificates";
+import Projects from "./Projects";
 
 export default function Main() {
     return (
         <main className="main-content">
             <About />
             <Certificates />
+            <Projects />
         </main>
     )
 }
