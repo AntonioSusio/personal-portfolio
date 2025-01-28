8e461ee7a1a4f51e74fb1485f6b0fefdf8c4e817
Antonio <antoniosusio@gmail.com>
2025-01-28 11:52:44 +0100

    fixed header import in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Header from "../components/Sections/Header";
+import Header from "../components/Header";
 import Main from "../components/Sections/Main";
 
 export default function App() {
