af7a34001ee964231bc17363d262529344081e07
Antonio <antoniosusio@gmail.com>
2025-02-05 21:02:49 +0100

    Layout total review and developed final version

--- projectsData.js
+++ projectsData.js
@@ -2,10 +2,9 @@ export default [
     {
         id: "1",
         projectName: "Vanlife app",
-        description: "A website project based on the buying and selling "+
-        "of camper vans, built with the help of React and Javascript, also using "+
-        "the React Routing method to navigate between pages and Auth Required for the login "+
-        "necessary to access sections reserved only for logged-in users",
+        description: "A website project based on the buying and selling of camper vans. "+
+        "React Routing method was used to navigate between pages and Authentication Required to access sections "+
+        "reserved only for logged-in users.",
         imagePath: "./src/assets/images/VanLifeApp.png",
         alternativeText: "image representing Vanlife project home screen",
         url: "https://van-life-antonio.netlify.app",
@@ -15,8 +14,8 @@ export default [
     {
         id: "2",
         projectName: "Quizzical App",
-        description: "A simple quiz app project built using React and JavaScript, "+
-        "where you have to answer 5 general knowledge questions.",
+        description: "A simple quiz app project "+
+        "where you have to answer five general knowledge questions.",
         imagePath: "./src/assets/images/QuizzicalApp.png",
         alternativeText: "image representing Quizzical project home screen",
         url: "https://quizzical-app-antonio.netlify.app",
@@ -27,7 +26,7 @@ export default [
         id: "3",
         projectName: "Movie Watchlist App",
         description: "A Watchlist application project where you can create " +
-        "your own list of movies or TV shows to watch, built using only JavaScript.",
+        "your own list of movies or TV shows to watch.",
         imagePath: "./src/assets/images/MovieWatchlistApp.png",
         alternativeText: "image representing Movie watchlist project home screen",
         url: "https://movie-watchlist-antonio.netlify.app",
@@ -37,9 +36,9 @@ export default [
     {
         id: "4",
         projectName: "Assembly Endgame",
-        description: "A project similar to the Hangman game, built using React and Javascript, where you have 8 lives, " +
-        "with one difference: your lives will be programming technologies, so try not to " + 
-        "lose your favorite technology and avoid letting assembly dominate the programming world. 😆",
+        description: "This project is very similar to the game of Hangman. "+
+        "You have 8 lives. Each life is represented by a different programming technology. "+
+        "Try not to lose your favorite!",
         imagePath: "./src/assets/images/AssemblyEndgameApp.png",
         alternativeText: "image representing  Assembly Endgame project home screen",
         url: "https://assembly-endgame-antonio.netlify.app",
@@ -49,8 +48,8 @@ export default [
     {
         id: "5",
         projectName: "Tenzies Game",
-        description: "A project built with React and JavaScript that represents "+
-        "a game where you have to win by finding the same number on all the dice. Will you be lucky quickly?",
+        description: "A project that represents "+
+        "a game where you have to win by finding the same number on all the dices. Will you be lucky quickly?",
         imagePath: "./src/assets/images/TenziesGameApp.png",
         alternativeText: "image representing Tenzies game project home screen",
         url: "https://tenzies-game-antonio.netlify.app",
@@ -60,7 +59,7 @@ export default [
     {
         id: "6",
         projectName: "Meme Generator",
-        description: "A simple project built with React that generates random memes and allows " +
+        description: "A simple project that generates random memes and allows " +
         "you to write what you'd like to be shown on the image. Let your creativity flow! 😆",
         imagePath: "./src/assets/images/MemeGeneratorApp.png",
         alternativeText: "image representing Meme generator project home screen",
