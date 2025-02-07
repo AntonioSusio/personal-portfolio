3751a2838dcfe01fb3b5d274043a5289e0607325
Antonio <antoniosusio@gmail.com>
2025-02-07 14:13:53 +0100

    Update some comment in the code

--- src/i18n.js
+++ src/i18n.js
@@ -1,20 +1,20 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-// Importa i file di traduzione
+// Import translation's file
 import enTranslations from './locals/en/translation.json';
 import itTranslations from './locals/it/translation.json';
 
 i18n
-  .use(initReactI18next) // Inizializza i18next per React
+  .use(initReactI18next) // Initialize i18next for React
   .init({
     resources: {
       en: { translation: enTranslations },
       it: { translation: itTranslations },
     },
-    lng: 'en',  // Impostato su inglese di default
-    fallbackLng: 'en',  // In caso di traduzione mancante, usa l'inglese
-    interpolation: { escapeValue: false }, // React non ha bisogno di escape dei valori
+    lng: 'en',  // Setted on EN by default
+    fallbackLng: 'en',  // In case there is no traduction, use english
+    interpolation: { escapeValue: false }, // React doesn't need escape for value
   });
 
 export default i18n;
