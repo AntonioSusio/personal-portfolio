6f126e79b298dd1e1fe5f5f7f1b594524f88939d
Antonio <antoniosusio@gmail.com>
2025-02-08 12:20:26 +0100

    try fix 2

--- components/Sections/About.jsx
+++ components/Sections/About.jsx
@@ -2,7 +2,6 @@ import React from "react"
 import data from "../../data";
 import { FaLinkedinIn, FaGithub } from "react-icons/fa";
 import { useTranslation } from 'react-i18next';
-import AboutMeImg from "../../src/assets/images/AboutMeImg.webp"
 
 export default function About() {
     const { t } = useTranslation();
@@ -11,7 +10,7 @@ export default function About() {
         <section className="about-section">
                 <div className="card-container">
                     <div className="card-image-container">
-                        <img src={AboutMeImg} alt="An image representing Antonio watching in the camera smiling" />
+                        <img src={data.imagePath} alt="An image representing Antonio watching in the camera smiling" />
                     </div>
 
                     <div className="contacts-container">
--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 export default {
     fullName: "Antonio Susio",
-    image: "./src/assets/imagesAboutMeImg.webp",
+    imagePath: "./src/assets/imagesAboutMeImg.webp",
     email: "antoniosusio@gmail.com",
     skills: ["Html", "Css", "Javascript", "React", "Figma"]
 }
